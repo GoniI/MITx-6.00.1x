736a38136cee4f296fca8fa0ea778926fd3212c3
ayruss <gon.istogu@gmail.com>
2018-02-16 03:44:24 +0100

    Changes to be committed:

--- p1.py
+++ p1.py
@@ -1,3 +1,4 @@
+#testing git
 # problem 1
 #Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'.
 s = 'alsisjsissdab'
@@ -6,3 +7,5 @@ for letter in s:
     if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
         vowels += 1
 print ("Number of vowels: " + str(vowels))
+
+
